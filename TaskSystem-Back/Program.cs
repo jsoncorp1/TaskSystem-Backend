bf79b98660885b1c8543ea3b2d74eaf8bfd16023
HaroldCorp <jsoncorp1@gmail.com>
2026-07-03 22:53:17 -0400

    feat: solve problems with order in authentiation

--- TaskSystem-Back/Program.cs
+++ TaskSystem-Back/Program.cs
@@ -84,7 +84,7 @@ if (app.Environment.IsDevelopment())
 //-----------------------------------------------------
 app.UseHttpsRedirection();
 app.UseCors("AllowAll");
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
